0cbe278feb0b00583f786b5e3ffaaae48afb8e70
Kevin Trebossen <Kevin@macbook-pro-de-kevin.local>
2018-09-10 12:47:20 +0200

    Création des répertoires MVC

---
+++ MonRestoDuMonde/Controleur/MenuController.swift
@@ -0,0 +1,16 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by Kevin Trebossen on 10/09/18.
+//  Copyright © 2018 KTD. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
