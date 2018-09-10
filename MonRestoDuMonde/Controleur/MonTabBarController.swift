51b512b1709af0683c2e7a2fff2aa506d4e02d7b
Kevin Trebossen <Kevin@macbook-pro-de-kevin.local>
2018-09-10 20:38:02 +0200

    Ajout des constantes + TabBarCustom

---
+++ MonRestoDuMonde/Controleur/MonTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  MonRestoDuMonde
+//
+//  Created by Kevin Trebossen on 10/09/18.
+//  Copyright © 2018 KTD. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+    }
+
+
+}
---
+++ MonRestoDuMonde/Utilitaire/Constantes.swift
@@ -0,0 +1,13 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by Kevin Trebossen on 10/09/18.
+//  Copyright © 2018 KTD. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black        // Constantes en MAJUSCULES
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)     // NOIR -> 0 / 255
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)  // BLANC -> 255 / 255
