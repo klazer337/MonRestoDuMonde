866e4c706f73a165fc93e2188d0197979fb5acb3
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-12 23:47:42 +0200

    Ajout de la Classe Menu, LesPlats et ajout des pays ans les ENUMS

--- MonRestoDuMonde/Controleur/MenuController.swift
+++ MonRestoDuMonde/Controleur/MenuController.swift
@@ -14,6 +14,7 @@ class MenuController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     let cellId = "MenuCell"
     
+    var menus = [Menu]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +23,12 @@ class MenuController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         collectionView.delegate = self
         collectionView.dataSource = self
-        
+        menus = LesPlats.obtenir.lesMenus()
+        collectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return menus.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
--- MonRestoDuMonde/Utilitaire/Constantes.swift
+++ MonRestoDuMonde/Utilitaire/Constantes.swift
@@ -12,3 +12,12 @@ let NOIR = UIColor.black        // Constantes en MAJUSCULES
 let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)     // NOIR -> 0 / 255
 let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)  // BLANC -> 255 / 255
 let FONT_DE_BASE = UIFont(name: "Chalkduster", size: 20)
+
+
+let ITALIE = "Italien"
+let JAPON = "Japonais"
+let CHINE = "Chinois"
+let USA = "Américain"
+let MEXIQUE = "Mexicains"
+let ESPAGNE = "Espagnol"
+let SUISSE = "Suisse"
