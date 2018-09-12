aa4673df14ec5c39b9de1b2f5dff3de2fcc24b70
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-12 23:17:50 +0200

    Class Plats et Enums -> OK

---
+++ MonRestoDuMonde/Modele/Enums.swift
@@ -0,0 +1,17 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by Kevin Trebossen on 12/09/18.
+//  Copyright © 2018 KTD. All rights reserved.
+//
+
+import Foundation
+
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+}
+
+
