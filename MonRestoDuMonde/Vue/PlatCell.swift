d571469d273ef235fd8e17b25bc8926514f0f4c2
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-15 22:42:22 +0200

    Mise en place du UITableViewCellController

---
+++ MonRestoDuMonde/Vue/PlatCell.swift
@@ -0,0 +1,35 @@
+//
+//  PlatCell.swift
+//  MonRestoDuMonde
+//
+//  Created by Kevin Trebossen on 15/09/18.
+//  Copyright © 2018 KTD. All rights reserved.
+//
+
+import UIKit
+
+class PlatCell: UITableViewCell {
+
+    @IBOutlet weak var imageDuPlat: UIImageView!
+    @IBOutlet weak var descDuPlat: UILabel!
+    
+    var plat: Plat
+    
+    func miseEnPlace (plat: Plat){
+        self.plat = plat
+        
+        imageDuPlat.image = self.plat.image
+        imageDuPlat.contentMode = .scaleAspectFill
+        imageDuPlat.clipsToBounds = true
+        imageDuPlat.layer.cornerRadius = 20
+        
+        descDuPlat.text = self.plat.desc
+        descDuPlat.textAlignment = .center
+        descDuPlat.numberOfLines = 0
+        descDuPlat.font = FONT_DE_BASE
+        descDuPlat.textColor = GRIS_TRES_CLAIR
+        
+    }
+    
+
+}
