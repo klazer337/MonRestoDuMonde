39c5b32c6ff07de076676eba7b2321798202f08b
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-15 21:53:33 +0200

    Création UIMenuCell

--- MonRestoDuMonde/Vue/MenuCell.swift
+++ MonRestoDuMonde/Vue/MenuCell.swift
@@ -9,5 +9,34 @@
 import UIKit
 
 class MenuCell: UICollectionViewCell {
+    @IBOutlet weak var vue: UIView!
+    @IBOutlet weak var nomDuMenu: UILabel!
+    @IBOutlet weak var imageDuMenu: UIImageView!
+    
+    var menu: Menu!
+    
+    func miseEnPlace(menu: Menu){
+        self.menu = menu
+        
+        vue.layer.cornerRadius = 10 // Bords arrondis
+        vue.layer.borderColor = NOIR.cgColor
+        vue.layer.borderWidth = 1
+        
+        imageDuMenu.image = self.menu.image
+        imageDuMenu.contentMode = .scaleAspectFit
+        imageDuMenu.clipsToBounds = true                // On garde la taille sans que ca déborde
+        imageDuMenu.layer.cornerRadius = imageDuMenu.frame.width / 2
+        imageDuMenu.layer.borderWidth = 2
+        imageDuMenu.layer.borderColor = GRIS_TRES_CLAIR.cgColor
+        
+        nomDuMenu.text = "Menu " + self.menu.pays
+        nomDuMenu.textColor = GRIS_TRES_FONCE
+        nomDuMenu.font = FONT_DE_BASE
+        nomDuMenu.adjustsFontSizeToFitWidth = true        // Ajuster la taille de la font
+        nomDuMenu.textAlignment = .center
+        
+        
+    }
+    
     
 }
