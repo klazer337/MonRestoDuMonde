4d036645e4ed419bf01bcdfc2c69fdd2cb279ad3
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-16 00:19:20 +0200

    Fin de l'application

--- MonRestoDuMonde/Controleur/DetailController.swift
+++ MonRestoDuMonde/Controleur/DetailController.swift
@@ -18,11 +18,36 @@ class DetailController: UIViewController {
         super.viewDidLoad()
         view.layer.addSublayer(Degrade())   // Ajout du degradé
         view.bringSubview(toFront: scroll)  // ajouter la scroll view au premier plan
-        guard menu != nil else {return miseEnPlace(menu: menu!)}
+        guard menu != nil else { return }
+        miseEnPlace(menu: menu!)
     }
     
     func miseEnPlace(menu: Menu){
-        print("Ca marche on a le menu : \(menu.pays)")
+        let imageDuMenu = UIImageView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 250))
+        imageDuMenu.image = menu.image
+        imageDuMenu.contentMode = .scaleAspectFill
+        imageDuMenu.clipsToBounds = true
+        scroll.addSubview(imageDuMenu)
+        var maxY = imageDuMenu.frame.maxY
+        let nomDuMenu = UILabel(frame: CGRect(x: 0, y: maxY, width: view.frame.width, height: 100))
+        nomDuMenu.text = "Menu " + menu.pays
+        nomDuMenu.font = FONT_TITRE
+        nomDuMenu.textColor = .red
+        nomDuMenu.adjustsFontSizeToFitWidth = true
+        nomDuMenu.textAlignment = .center
+        scroll.addSubview(nomDuMenu)
+        maxY = nomDuMenu.frame.maxY
+        
+        for plat in menu.plats {
+            let vue = PlatVue(frame: CGRect(x: 0, y: maxY, width: view.frame.width, height: 375))
+            vue.miseEnPlace(plat: plat)
+            scroll.addSubview(vue)
+            maxY = vue.frame.maxY
+        }
+        
+        scroll.contentSize = CGSize(width: view.frame.width, height: maxY + 50)
+        
+        
     }
 
 }
---
+++ MonRestoDuMonde/Vue/PlatVue.swift
@@ -0,0 +1,67 @@
+//
+//  PlatVue.swift
+//  MonRestoDuMonde
+//
+//  Created by Kevin Trebossen on 15/09/18.
+//  Copyright © 2018 KTD. All rights reserved.
+//
+
+import UIKit
+
+class PlatVue: UIView {
+    
+    var typeDePlat: UILabel?
+    var imageDuPlat: UIImageView?
+    var descriptionDuPlat: UILabel?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func miseEnPlace(plat: Plat) {
+        typeDePlat = UILabel(frame: CGRect(x: 20, y: 0, width: frame.width - 40, height: 50))
+        typeDePlat?.text = convertirTypeRnPlat(type: plat.type)
+        typeDePlat?.textAlignment = .left
+        typeDePlat?.font = FONT_DE_BASE
+        typeDePlat?.textColor = GRIS_TRES_CLAIR
+        guard typeDePlat != nil else { return }
+        addSubview(typeDePlat!)
+        var maxY = typeDePlat!.frame.maxY
+        
+        imageDuPlat = UIImageView(frame: CGRect(x: 0, y: maxY, width: frame.width, height: 200))
+        imageDuPlat?.image = plat.image
+        imageDuPlat?.contentMode = .scaleAspectFill
+        imageDuPlat?.clipsToBounds = true
+        guard imageDuPlat != nil else { return }
+        addSubview(imageDuPlat!)
+        maxY = imageDuPlat!.frame.maxY
+        
+        descriptionDuPlat = UILabel(frame: CGRect(x: 20, y: maxY, width: frame.width - 40, height: 100))
+        descriptionDuPlat?.numberOfLines = 0
+        descriptionDuPlat?.text = plat.desc
+        descriptionDuPlat?.font = FONT_DE_BASE
+        descriptionDuPlat?.textColor = GRIS_TRES_CLAIR
+        descriptionDuPlat?.textAlignment = .center
+        guard descriptionDuPlat != nil else { return }
+        addSubview(descriptionDuPlat!)
+        
+    }
+    
+    func convertirTypeRnPlat(type: Type) -> String {
+        switch type {
+        case .entree: return "Entrée"
+        case .platPrincipal: return "Plat principal"
+        case .dessert: return "Dessert"
+
+        }
+    }
+    
+    
+    
+    
+    
+}
